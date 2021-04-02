4f599f0039e9c7e5b88ed2476fc5b7f25a21ab3d
tglukhov <33P73g6SEzb8>
2021-04-02 16:09:00 +0400

    Прорабатываю функционал

--- script.js
+++ script.js
@@ -1,26 +1,57 @@
-const subTaskTitle = document.querySelector('.subtask_name');
-const subTaskHours = document.querySelector('.subtask_hours');
+const TaskName = document.querySelector('.task_name');
+let subTaskTitle;
+let subTaskHours;
 
 
 function SubTask(title, hours) {
-  this.title = subTaskTitle;
-  this.hours = subTaskHours;
+  this.title = title;
+  this.hours = hours;
 };
 
-let subTask = new SubTask('title');
-console.log(subTask);
+const btn = document.querySelector('.create_task');
 
+
+
+btn.onclick = () => {
+  subTaskTitle = document.getElementById('subtask-name').value;
+  subTaskHours = document.querySelector('.subtask_hours').value;
+  //console.log(subTask);
+  //return subTask;
+  let subTask = new SubTask(subTaskTitle, subTaskHours);
+  console.log(subTask);
+};
 
 function Task(title) {
-  this.title = "Новая задача";
-  this.subTaskList = [subTask];
-  this.createSubTask = function() {};
-  this.deleteSubTask = function() {};
-}
-
-let task = new Task('title');
-
-let taskList = {
-  taskList: [task],
-  createTask(){},
+  this.title = title;
+  this.subTaskList = [];
+  this.addSubTask = function(subTask) {
+   this.subTaskList.push(subTask);
+  };
 };
+
+const taskList = [];
+const addTask = () => {};
+
+
+
+
+
+
+//let subTask98769876 = new SubTask('новая ' + 'подзадача', 1);
+
+//let task = new Task('новая задача');
+//let task2 = new Task('вторая задача');
+
+//task.addSubTask();
+//console.log(task.title);
+
+//task2.addSubTask(new SubTask('yjjhkjah', 43));
+
+
+
+
+
+//let taskList = {
+//  taskList: [task],
+//  createTask(){},
+//};
